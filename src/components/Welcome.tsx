a8124e85bdab89d194049c847ea1ab1620157433
Molly Brace <mollybrace@Mollys-MBP.home>
2023-12-09 14:28:00 +0000

    updating bug

--- src/components/Welcome.tsx
+++ src/components/Welcome.tsx
@@ -5,7 +5,7 @@ const Welcome = () => {
     // <div className='flex h-36 bg-twitter-blue'>Welcome</div>
     <section>
       <div className='h-32 bg-dogwood flex items-center justify-center'>
-      <h1 className='text-moonstone text-[34px]  font-roboto md:text-[50px]'>
+      <h1 className='text-moonstone text-[34px]  font-roboto md:text-[50px] sm:text-[40px] whitespace-nowrap'>
         Welcome to Mocean Therapy</h1>
 
       </div>
